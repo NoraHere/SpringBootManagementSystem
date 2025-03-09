55b4d9b647a095a443664f9437557f536424b644
JY <e1237248@u.nus.edu>
2025-03-10 00:58:42 +0800

    transactional (propagation) log any delete

--- src/main/java/com/practicen/service/impl/DeptServiceImpl.java
+++ src/main/java/com/practicen/service/impl/DeptServiceImpl.java
@@ -1,13 +1,18 @@
 package com.practicen.service.impl;
 
 import com.practicen.mapper.DeptMapper;
+import com.practicen.mapper.EmpMapper;
 import com.practicen.pojo.Dept;
+import com.practicen.pojo.DeptLog;
+import com.practicen.service.DeptLogService;
 import com.practicen.service.DeptService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @Slf4j
@@ -15,6 +20,12 @@ import java.util.List;
 public class DeptServiceImpl implements DeptService {
     @Autowired
     private DeptMapper deptMapper;
+
+    @Autowired
+    private EmpMapper empMapper;
+
+    @Autowired
+    private DeptLogService deptLogService;
 
     public void add(Dept dept) {
         //finish dept data
@@ -24,8 +35,22 @@ public class DeptServiceImpl implements DeptService {
         deptMapper.insert(dept);
     }
 
+    @Override
+    @Transactional(rollbackFor = Exception.class)
     public void delete(Integer id){
-        deptMapper.delete(id);
+        try {
+            deptMapper.delete(id); //根据ID删除部门数据
+
+            int i = 1/0;
+            //if(true){throw new Exception("出错啦...");}
+
+            empMapper.delete(new ArrayList<>(id)); //根据部门ID删除该部门下的员工
+        } finally {
+            DeptLog deptLog = new DeptLog();
+            deptLog.setCreateTime(LocalDateTime.now());
+            deptLog.setDescription("execute delete dept, id: "+id);
+            deptLogService.insert(deptLog);
+        }
     }
 
     public List<Dept> list(){
