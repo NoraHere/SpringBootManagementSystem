5a9f1485685218ddf1497b6e78b6452d32c19e1c
JY <e1237248@u.nus.edu>
2025-03-09 20:04:06 +0800

    login filter

--- src/main/java/com/practicen/SpringbootMybatisWebManagementApplication.java
+++ src/main/java/com/practicen/SpringbootMybatisWebManagementApplication.java
@@ -2,7 +2,9 @@ package com.practicen;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan
 @SpringBootApplication
 public class SpringbootMybatisWebManagementApplication {
 
